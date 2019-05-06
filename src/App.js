3856f3c6fe854bcad16b855eeea48c9ee0f4583e
Paul Hayes <water7earth@gmail.com>
2019-05-06 12:13:40 +0200

    Class syntax finalized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import React, { useState } from 'react';
-import useHackerNewsApi from './hooks/useHackerNewsApi';
+import React, { Component } from 'react';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
 import MainLayout from './layout/MainLayout';
 import ResultsList from './components/ResultsList/ResultsList';
 import './App.css';
 
-function App() {
-  const [query, setQuery] = useState('redux');
-  const { data, isLoading, isError } = useHackerNewsApi(query);
+class App extends Component {
+  state = {
+    query: 'redux',
+    data: {
+      hits: [{}],
+    },
+    isLoading: false,
+    isError: false,
+  };
 
-  return (
-    <MainLayout>
-      <input
-        className="query-input"
-        type="text"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-      />
-      <div className="spacer--small" />
-      <ResultsList query={query} hits={data.hits} isLoading={isLoading} isError={isError} />
-    </MainLayout>
-  );
+  constructor() {
+    super();
+    this.debouncedFetchData = debounce(this.fetchData, 500);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  handleChange = (event) => {
+    this.setState({ query: event.target.value });
+    this.debouncedFetchData();
+  }
+
+  fetchData = async () => {
+    const { query } = this.state;
+    this.setState({ isLoading: true });
+
+    try {
+      const result = await axios(
+        `https://hn.algolia.com/api/v1/search?query=${query}`,
+      );
+
+      this.setState({ data: result.data });
+    } catch (error) {
+      this.setState({ isError: true });
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+
+  render() {
+    const {
+      query, data, isLoading, isError,
+    } = this.state;
+    return (
+      <MainLayout>
+        <input
+          className="query-input"
+          type="text"
+          name="query"
+          value={query}
+          onChange={this.handleChange}
+        />
+        <div className="spacer--small" />
+        <ResultsList query={query} hits={data.hits} isLoading={isLoading} isError={isError} />
+      </MainLayout>
+    );
+  }
 }
 
 export default App;
